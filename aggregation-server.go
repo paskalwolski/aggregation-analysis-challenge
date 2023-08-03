49a47eac2606f08a8484c29788dae23bae99359f
Paskal Wolski <paskal.wolski@gmail.com>
2023-08-03 12:27:35 +0200

    Added simple endpoint handler with basic response

--- aggregation-server.go
+++ aggregation-server.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Starting the Aggregation Server")
 
+	http.HandleFunc("/analysis", handleGetAnalysis)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func handleGetAnalysis(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Success"))
 }
