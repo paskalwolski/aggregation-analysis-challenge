c4d5a44c84ec9fa50c3b17883a763ebd23409075
Paskal Wolski <paskal.wolski@gmail.com>
2023-08-09 21:12:39 +0200

    Added dimension and data trackers to calculate average Added float64-based reading of dimension data from JSON stream Quickly caluculated dimesion average Updated readme - issues

--- utilities.go
+++ utilities.go
@@ -48,6 +48,8 @@ func HandleAnalysisQuery(dur, dim string) (aResponse AnalysisResponse, aError er
 
 	// Create a buffered reader for the SSE response
 	scanner := bufio.NewScanner(resp.Body)
+	// Create counters for analysis
+	var dimensionCounter, dataCounter float64 = 0, 0
 
 ScanLoop:
 	for scanner.Scan() {
@@ -57,19 +59,44 @@ ScanLoop:
 			break ScanLoop
 		default:
 			text := scanner.Text()
+			// Catch empty lines between messages
 			if text != "" {
+				dataCounter++
+				// Append opening { to json line
 				trimmedText := fmt.Sprintf("{%v", text[7:])
-				fmt.Println(trimmedText)
-				var m map[string]any
-				err = json.Unmarshal([]byte(trimmedText), &m)
+				var root map[string]map[string]any
+				// store the {post_type: data} object
+				err = json.Unmarshal([]byte(trimmedText), &root)
 				if err != nil {
 					fmt.Println(err)
 				}
-
+				// Check for the 'data' key
+				for k, v := range root {
+					// default numeric value from extracting map[]any is float64
+					if dimData, exists := v[dim]; exists {
+						fmt.Printf("Found data %v of %T: %v\n", dim, dimData, dimData)
+						// Converting dimension data to explicit float64. This has to be done first?
+						var dimFloatVal float64
+						var ok bool
+						// Could switch dimension, and then assert accordingly - but only asserting to float64 for now.
+						dimFloatVal, ok = dimData.(float64)
+						if !ok {
+							fmt.Println("Error converting to float64")
+						}
+						dimensionCounter += dimFloatVal
+					} else {
+						fmt.Printf("Key Not Found: %v on type %v\n", dim, k)
+					}
+				}
 			}
 		}
 	}
 	fmt.Printf("Reading Request for %v\n", time.Since(startTime))
+
+	fmt.Printf("Analysed %v messages\n", dataCounter)
+	fmt.Printf("Dimension has a total value of %v\n", dimensionCounter)
+	fmt.Printf("Mean: %v\n", float32(dimensionCounter/dataCounter))
+
 	if scanner.Err() != nil {
 		aError = scanner.Err()
 		return
