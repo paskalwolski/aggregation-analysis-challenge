17871a887a70ed52d83084ce0f0619a57f2c212e
Paskal Wolski <paskal.wolski@gmail.com>
2023-09-11 09:30:18 +0200

    Created sub id from mutex timestamp

--- aggregation-server.go
+++ aggregation-server.go
@@ -5,26 +5,30 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 )
 
 type subscriber struct {
+	id       int64
 	duration time.Duration
 	key      string
 	value    int
 	channel  chan stats
 }
 type publisher struct {
-	subscribers  []subscriber
+	subscribers  []*subscriber
 	targetStream *http.Response
+	mu *sync.Mutex
 }
 
 func main() {
 	fmt.Println("Starting the Aggregation Server")
 	// Creating the publisher
 	pub := publisher{
-		make([]subscriber, 0),
+		make([]*subscriber, 0),
 		getSSEResponse(),
+		&sync.Mutex{},
 	}
 
 	http.HandleFunc("/analysis", func(w http.ResponseWriter, r *http.Request) {
@@ -48,13 +52,13 @@ func handleSubscriberWithResource(w http.ResponseWriter, r *http.Request, pub *p
 		}
 		dur, err := time.ParseDuration(duration)
 
-		sub := newSub(dur, dimension)
+		sub := newSub(dur, dimension, pub.mu)
 		pub.addSubscriber(sub)
 		boom := time.After(dur)
 		select {
 		case <-boom:
 			fmt.Printf("Time Channel Closed")
-			pub.unSub(&sub)
+			pub.unSub(sub)
 		}
 
 	} else {
@@ -62,21 +66,29 @@ func handleSubscriberWithResource(w http.ResponseWriter, r *http.Request, pub *p
 	}
 }
 
-func (p *publisher) addSubscriber(s subscriber) {
+func (p *publisher) addSubscriber(s *subscriber) {
 	p.subscribers = append(p.subscribers, s)
 }
 
-func (p *publisher) unSub(s *subscriber){
+func (p *publisher) unSub(s *subscriber) {
 	// remove the sub  from the pub's sub list
 }
 
-func newSub(dur time.Duration, dim string) (s subscriber) {
-	return subscriber{
+func newSub(dur time.Duration, dim string, mu *sync.Mutex) (s *subscriber) {
+	return &subscriber{
+		createNewSubId(mu),
 		dur,
 		dim,
 		0,
 		make(chan stats),
 	}
+}
+
+func createNewSubId(mu *sync.Mutex) (id int64) {
+	mu.Lock()
+	id = time.Now().Unix()
+	mu.Unlock()
+	return
 }
 
 func handleGetAnalysis(w http.ResponseWriter, r *http.Request) {
