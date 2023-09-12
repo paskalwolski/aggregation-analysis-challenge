442ee5b1af05a77a5c9fb8c292d239c9e9e4bbb2
Paskal Wolski <paskal.wolski@gmail.com>
2023-09-12 10:52:29 +0200

    Removed previous root handle function Added util functions to identify event types Added parsing logic for data events Added docstrings to newSub and runPub handlers

--- aggregation-server.go
+++ aggregation-server.go
@@ -15,7 +15,8 @@ type subscriber struct {
 	duration time.Duration
 	key      string
 	value    int
-	channel  chan string
+	channel  chan map[string]interface{}
+	open     bool
 }
 type publisher struct {
 	subscribers  []*subscriber
@@ -42,6 +43,10 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+// Publisher main thread.
+//
+// This interprets the incoming SSE stream, and parses data events into a map.
+// The map is sent to the list of subscribers.
 func runPub(p *publisher) {
 	scanner := bufio.NewScanner(p.targetStream.Body)
 	// Create counters for analysis
@@ -50,14 +55,46 @@ func runPub(p *publisher) {
 	// var minTime, maxTime time.Time
 
 	for scanner.Scan() {
-		t := scanner.Text()
-		// fmt.Println(scanner.Text())
-		p.subsMu.RLock()
-		for _, s := range p.subscribers {
-			s.channel <- t
+		t := scanner.Bytes()
+		event, body := identifyStreamData(t)
+		switch event {
+		case "data":
+			data := decomposeDataEvent(body)
+			p.subsMu.RLock()
+			for _, s := range p.subscribers {
+				if s.open {
+					s.channel <- data
+				}
+			}
+			p.subsMu.RUnlock()
+		case "message":
+			//Custom handling of SSE Message event
+			fmt.Println("Encountered Message Event")
+		default:
+			// Unkown event encountered = discard
+			continue
 		}
-		p.subsMu.RUnlock()
 	}
+}
+
+func identifyStreamData(message []byte) (event string, body []byte) {
+	if message == nil {
+		return "", nil
+	}
+	for i, b := range message {
+		if b == ':' {
+			event = string(message[:i])
+			body = message[i+1:]
+			break
+		}
+	}
+	return
+}
+
+func decomposeDataEvent(text []byte) (data map[string]interface{}) {
+	json.Unmarshal(text, &data)
+	fmt.Println(data)
+	return
 }
 
 func handleSubscriberWithResource(w http.ResponseWriter, r *http.Request, pub *publisher) {
@@ -77,7 +114,9 @@ func handleSubscriberWithResource(w http.ResponseWriter, r *http.Request, pub *p
 
 		sub := newSub(dur, dimension, pub.subscribeMu)
 		pub.addSubscriber(sub)
+		// Add this sub to the pub's list
 		go sub.getSubData()
+		// Start the clock!
 		boom := time.After(dur)
 		<-boom
 		fmt.Printf("Time Channel Closed\n")
@@ -89,7 +128,8 @@ func handleSubscriberWithResource(w http.ResponseWriter, r *http.Request, pub *p
 }
 
 func (s *subscriber) getSubData() {
-	for v := range s.channel{
+	for v := range s.channel {
+		// sub will handle per-event processing here
 		fmt.Println(v)
 	}
 }
@@ -101,6 +141,8 @@ func (p *publisher) addSubscriber(s *subscriber) {
 }
 
 func (p *publisher) unSub(sub *subscriber) {
+	//First 'lock' the sub so that it stops receiving
+	sub.open = false
 	// remove the sub  from the pub's sub list
 	for i, s := range p.subscribers {
 		if s.id == sub.id {
@@ -122,12 +164,17 @@ func newSub(dur time.Duration, dim string, mu *sync.Mutex) (s *subscriber) {
 		dur,
 		dim,
 		0,
-		make(chan string),
+		make(chan map[string]any),
+		true,
 	}
 	fmt.Printf("Created new sub with ID %v\n", s.id)
 	return
 }
 
+// Create a new unique sub id using the timestamp.
+// This timestamp is a mutex, ensuring uniqueness
+//
+// Could be an atomic counter, but oh well
 func createNewSubId(mu *sync.Mutex) (id int64) {
 	mu.Lock()
 	id = time.Now().Unix()
@@ -135,34 +182,34 @@ func createNewSubId(mu *sync.Mutex) (id int64) {
 	return
 }
 
-func handleGetAnalysis(w http.ResponseWriter, r *http.Request) {
+// func handleGetAnalysis(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method == "GET" {
-		q, err := url.ParseQuery(r.URL.RawQuery)
-		if err != nil {
-			http.Error(w, "invalid query data", 400)
-			return
-		}
-		dimension := q.Get("dimension")
-		duration := q.Get("duration")
-		if dimension == "" || duration == "" {
-			http.Error(w, "invalid query value", 400)
-			return
-		}
-		res, err := HandleAnalysisQuery(duration, dimension)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		jsonRes, err := json.Marshal(res)
-		if err != nil {
-			http.Error(w, "error creating response", 500)
-			return
-		}
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(jsonRes)
-	} else {
-		http.Error(w, "method not supported", 404)
-	}
-}
+// 	if r.Method == "GET" {
+// 		q, err := url.ParseQuery(r.URL.RawQuery)
+// 		if err != nil {
+// 			http.Error(w, "invalid query data", 400)
+// 			return
+// 		}
+// 		dimension := q.Get("dimension")
+// 		duration := q.Get("duration")
+// 		if dimension == "" || duration == "" {
+// 			http.Error(w, "invalid query value", 400)
+// 			return
+// 		}
+// 		res, err := HandleAnalysisQuery(duration, dimension)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), 500)
+// 			return
+// 		}
+// 		jsonRes, err := json.Marshal(res)
+// 		if err != nil {
+// 			http.Error(w, "error creating response", 500)
+// 			return
+// 		}
+// 		w.Header().Add("Content-Type", "application/json")
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write(jsonRes)
+// 	} else {
+// 		http.Error(w, "method not supported", 404)
+// 	}
+// }
